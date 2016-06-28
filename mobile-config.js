5b2ae5497f41f4d3d08c55cb0cbf6110f80d4966
John Pinkerton <jpinkerton88@gmail.com>
2016-06-28 16:24:18 -0400

    init android build

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+App.info({
+  id: 'cc.newspring.LaunchBasicExample',
+  name: 'LaunchBasicExample',
+  description: 'Example of using launch',
+  author: 'NewSpring Church',
+  email: 'web@newspring.cc',
+  website: 'https://newspring.cc',
+  version: '0.0.1',
+  buildNumber: '1'
+});
